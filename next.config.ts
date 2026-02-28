26e826c7edfc16078b83d394a07057dc340150ab
raushan <rk9939551166@gmail.com>
2026-02-28 18:02:25 +0530

    git deploy test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 1. Tells Next.js to build static HTML files
+  output: 'export',
+  
+  // 2. Disables Next.js server-side image optimization (required for static exports)
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
